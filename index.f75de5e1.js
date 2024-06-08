0bfaf6482d4cd854e08ced4201cf57d477b161cf
Dmytro Bashlai <imaxs2016@gmail.com>
2024-06-09 00:03:37 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,23 @@
+"use strict";
+const employees = [
+    ...document.querySelectorAll("li")
+];
+function parseSalaryString(salary) {
+    return +salary.replace(/[^0-9.-]+/g, "");
+}
+function sortList(list) {
+    return list.sort((li1, li2)=>parseSalaryString(li2.dataset.salary) - parseSalaryString(li1.dataset.salary)).forEach((li)=>document.querySelector("ul").append(li));
+}
+function getEmployees(list) {
+    return list.map((employee)=>{
+        return {
+            position: employee.dataset.position,
+            salary: employee.dataset.salary,
+            age: employee.dataset.age
+        };
+    });
+}
+sortList(employees);
+getEmployees(employees);
+
+//# sourceMappingURL=index.f75de5e1.js.map
